bb0a80240b867d19bc7978ec8023e8bd84bbd099
Adriean Khisbe <adriean.khisbe@live.fr>
2019-01-04 10:20:34 +0100

    add forgotten renamed test

---
+++ test/datadog-js.test.js
@@ -0,0 +1,11 @@
+describe("We need to test our API", function() {
+
+    it("Wait for the next release, :D", function() {
+        // very ugly
+        expect(true).toBe(true);
+    });
+    it("If there is one ^^", function() {
+        // very ugly
+        expect(true).toBe(true);
+    });
+});
